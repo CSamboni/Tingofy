6ea64f14070c9baad6b2dbd31f03d0cf3e0f6f67
jesussandrea <jesussandrea12@gmail.com>
2018-01-19 14:06:19 -0400

    Mensajeria funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    // Firebase
+    AngularFireModule.initializeApp(environment.firebase, 'talkiefy'),
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB29LGAhBOaYPU0DZL0ER1s28_lcVvQQGY',
+    authDomain: 'talkiefy.firebaseapp.com',
+    databaseURL: 'https://talkiefy.firebaseio.com',
+    projectId: 'talkiefy',
+    storageBucket: 'talkiefy.appspot.com',
+    messagingSenderId: '989764695037'
+  }
 };
