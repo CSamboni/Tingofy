7ee393232233ea7441933a46c6f3d508be8a9be5
jesus sandrea <jesussandrea12@gmail.com>
2018-02-06 10:59:51 -0500

    preparing before the desaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
-import { FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
-
-import * as firebase from 'firebase/app';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items: FirebaseListObservable<any[]>;
-  name: any;
-  msgVal: string;
-  displayName: string;
-  // messages = null;
-
-  constructor(public afAuth: AngularFireAuth, db: AngularFireDatabase) {
-    this.items = db.list('messages/').valueChanges();
-    this.messages = db.list('messages/')
-
-    this.afAuth.authState.subscribe(auth => {
-      if (auth) {
-        // this.user = this.db.object('users/' + auth.uid);
-        this.name = auth;
-      }
-    });
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
-  }
-
-  logout() {
-     this.afAuth.auth.signOut();
-  }
-
-  chatSend(theirMessage: string) {
-    this.messages.push({ message: theirMessage });
-    this.msgVal = '';
-  }
-
-  // chatSend(theirMessage: string) {
-  //   this.db.database.ref('messages/'+lugar.id).set(lugar);
-  // }
-
+  constructor() { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,36 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ChatComponent } from './chat/chat.component';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'chat', component: ChatComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    HomeComponent,
+    SignupComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     // Firebase
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(environment.firebase, 'talkiefy'),
     AngularFireModule,
     AngularFirestoreModule,
